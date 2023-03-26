833db58802aae0e4df658a602f02f61a9b18defb
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-26 20:57:32 +0100

    Added intro details and histogram code

--- analysis.py
+++ analysis.py
@@ -2,18 +2,32 @@
 # Author: Phelim Barry
 # Purpose: For analysis of iris data set
 
+#Import required modules
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
  
 #Read in the CSV file  and add column headers (might not need them but handy for when testing)
 iris_csv = pd.read_csv('iris.data', sep= ",", header=None)
 headers =  ["Sepal L", "Sepal W", "Petal L", "Petal W", "Class"]
 iris_csv.columns = headers
 
-#Displaying the contents of the CSV file
-print(iris_csv)
-print(iris_csv.to_string())
-print(iris_csv.loc[148])
-print(iris_csv.columns)
+#Create the file to store the results
+FILENAME = "analysis.txt"
+
+#Write an into to the results file
+with open(FILENAME, 'a') as f:
+     for_header = f.write("This fle shows the output of the analysis performd on the iris data set\n\n")
+
+
+#Histogram Code
+sepal_l = []
+for x in iris_csv['Sepal L']:
+    sepal_l.append(x)
+sepal_l.sort()
+print (sepal_l)
+plt.hist(sepal_l)
+plt.show()
 
 #playing with different print options
 for x in iris_csv['Sepal L']:
@@ -29,8 +43,6 @@ Max = iris_csv['Sepal L'].max()
 Min = iris_csv['Sepal L'].min()
 print(f"Min : {Min}, Max : {Max}, Sum : {Total}, Mean : {Mean}")
 
-#Create the file to store the results
-FILENAME = "analysis.txt"
 
 #Create a function for appending results
 def append_results():
